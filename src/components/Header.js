842caa0b554f2c913d6abc68d27b154e6500ec3e
unknown <marcinoles87@gmail.com>
2022-09-22 18:43:36 +0200

    Update LoginForm

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { StoreContext } from "../store/StoreProvider";
+import LoginForm from "./LoginForm/LoginForm";
 
 const Header = () => {
 
+    const [isModalOpen , setIsModalOpen] = useState(false);
     const {user,setUser} = useContext(StoreContext);
     const setProperlyLabel = Boolean(user) ? 'Wyloguj sie' : 'Zaloguj sie'
 
+    const handleOnClick = () => {
+        if(Boolean(user)) {
+            setUser(null);
+        }else {
+            setIsModalOpen(true);
+        }
+    }
+
+
+    const handleOnClose = () => setIsModalOpen(false);
     return ( 
         <header>
             <div className="logo"></div>
                 <h1 className="title">Super kursy dla programistow  !!!</h1>
                 <button>{setProperlyLabel}</button>
-            
+                <LoginForm handleOnClose={handleOnClose} isModalOpen={isModalOpen}/>
         </header>
      );
 }
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,4 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Modal from '../modal/modal'
 
 import { default as LoginFormStyle } from './LoginFormStyle' ;
 
+const LoginForm = (handleOnClose , isModalOpen) => {
+
+    const [login , setLogin ] = useState('');
+    const [password , setPassword ] = useState('');
+    const [validateMessage , setValidateMessage ] = useState('');
+
+    return(
+        <Modal handleOnClose={handleOnClose} isOpen={} shouldBeClosedOnOutsideClick={true} >
+            <form className="login" method="post" onSubmit={}>
+                <div className="row">
+                    <label>
+                        <input type="text" />
+                    </label>
+                </div>
+
+                <div className="row">
+                    <label>
+                        <input type="password" />
+                    </label>
+                </div>
+
+                <div className="row">
+                    <label>
+                        <button type="submit"></button>
+                        <button type="button"></button>
+                    </label>
+                </div>
+                
+            </form>
+        </Modal>
+    )
+};
+
+export default LoginForm;
