09f4436cb4d85eb1323b9db2f1905e3602014c69
unknown <marcinoles87@gmail.com>
2022-09-22 18:58:23 +0200

    add method to LoginForm

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import request from "../../helper/request";
+import { StoreContext } from "../../store/StoreProvider";
 
 import Modal from '../modal/modal'
 
@@ -10,25 +12,49 @@ const LoginForm = (handleOnClose , isModalOpen) => {
     const [password , setPassword ] = useState('');
     const [validateMessage , setValidateMessage ] = useState('');
 
+    const { setUser } = useContext(StoreContext);
+
+    const handleOnChangeLogin = ({target}) => setLogin(target.value);
+    const handleOnChangePassword = ({target}) => setPassword(target.value);
+    const handleOnCloseModal = event => {
+        event.preventDefault();
+        handleOnClose();
+
+    }
+
+    const handleOnSubmit = async event => {
+        event.preventDefault();
+        const {data , status } = await request.post(
+            '/users',
+            {login,password});
+    }
+
+    const validateMessageComponent = validateMessage.length
+    ? <p className="message">{validateMessage}</p> 
+    : null;
+
     return(
         <Modal handleOnClose={handleOnClose} isOpen={} shouldBeClosedOnOutsideClick={true} >
-            <form className="login" method="post" onSubmit={}>
+            {validateMessageComponent}
+            <form className="login" method="post" onSubmit={handleOnSubmit}>
                 <div className="row">
                     <label>
-                        <input type="text" />
+                        Login:
+                        <input onChange={handleOnChangeLogin} type="text" value={login} />
                     </label>
                 </div>
 
                 <div className="row">
                     <label>
-                        <input type="password" />
+                        Haslo:
+                        <input onChange={handleOnChangePassword} type="password" value={password} />
                     </label>
                 </div>
 
                 <div className="row">
                     <label>
-                        <button type="submit"></button>
-                        <button type="button"></button>
+                        <button  type="submit">Zaloguj</button>
+                        <button onClick={handleOnCloseModal} type="button">Anuluj</button>
                     </label>
                 </div>
                 
